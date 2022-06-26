a3c8d3d0454b8073a96cbb09ce67fe9f54b70dfe
Andrew Leck <Aleck360@gmail.com>
2022-06-26 15:50:10 -0500

    created a send back to home page button. Onclick of the button it will send you back to the home page

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,7 +1,21 @@
+import { useHistory } from 'react-router-dom';
+
 function MovieDetails() {
+    const history = useHistory()
+    
+    function sendBackToHomePage(){
+        console.log('inside send back to home page')
+        history.push('/');
+    }
+    
     return(
         <>
-            <h1>Details</h1>        
+            <h1>Details</h1>
+            <main>
+                <section>
+                    <button onClick={sendBackToHomePage}> Back to Movies </button>
+                </section>
+            </main>
         </>
     )
 }
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom'
 import './MovieList.css'
 
 function MovieList() {
+    const [movieDetails, setMovieDetail] = useState([]);
     const history = useHistory()
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
@@ -13,20 +14,27 @@ function MovieList() {
     }, []);
 
     //This function will show the details of the movie in new page
-    const showMeDetails = () => {
+function showMeDetails(){
         console.log('Inside show me details');
-        history.push('/details')
+        console.log('my list of movies are:', movies)
+        console.log(movieDetails)
+        history.push(`/details/${movieDetails}`)
+        dispatch({
+            type: 'GET_DETAILS',
+            payload: movieDetails
+        })
+
     }
 
     return (
         <main>
             <h1>MovieList</h1>
-            <section className="movies">
+            <section className="movies" >
                 {movies.map(movie => {
                     return (
-                        <div key={movie.id} onClick={showMeDetails} >
+                        <div key={movie.id}  onChange={evt => setMovieDetail(evt.target.value)}>
                             <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
+                            <img src={movie.poster} alt={movie.title} value={movieDetails} onClick={showMeDetails}/>
                         </div>
                     );
                 })}
